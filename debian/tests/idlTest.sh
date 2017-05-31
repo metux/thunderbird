b5136f736a58e0ad4c0b892d437e5142ace7d41d
Carsten Schoenert <c.schoenert@t-online.de>
2017-05-31 21:51:18 +0200

    autopkg: improve the output of idlTest.sh
    The tests which generate a file inside idlTest.sh doesn't have checked
    the existence of the generated files, adding some extra magic for that.

--- debian/tests/idlTest.sh
+++ debian/tests/idlTest.sh
@@ -20,13 +20,26 @@ echo -n "Test1: Make sure we can generate typelibs..."
     -o "${SCRATCHDIR}/test.xpt" \
     "${TEST_IDL_FILE}"
 echo "done."
+if [ -f "${SCRATCHDIR}/test.xpt" ]; then
+    echo "done."
+else
+    echo "No!"
+    echo "Test call successful but no outputfile '${SCRATCHDIR}/test.xpt' found!"
+	exit 1
+fi
 
 echo -n "Test2: Make sure we can generate C++ headers..."
 /usr/lib/thunderbird-devel/sdk/bin/header.py \
     -I"/usr/lib/thunderbird-devel/idl" \
     -o "${SCRATCHDIR}/test.h" \
     "${TEST_IDL_FILE}"
-echo "done."
+if [ -f "${SCRATCHDIR}/test.h" ]; then
+    echo "done."
+else
+    echo "No!"
+    echo "Test call successful but no outputfile '${SCRATCHDIR}/test.h' found!"
+	exit 1
+fi
 
 echo -n "Test3: Compiling generated file..."
 g++ -std=c++11 \
